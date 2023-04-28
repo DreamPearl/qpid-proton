ece9629381d137b177d595d4245ca02ac2e445a3
Rakhi Kumari <rakhi.cs61@gmail.com>
2023-04-28 17:21:23 +0530

    Replace jaeger exporter with otlp exporter

--- ci/otel.sh
+++ ci/otel.sh
@@ -24,12 +24,29 @@ sudo apt-get update
 # OTel dependencies
 
 sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install libboost-locale-dev
-sudo apt-get install libthrift-dev
+# sudo apt-get install libboost-locale-dev
+# sudo apt-get install libthrift-dev
+# sudo apt-get install libprotobuf-dev
+
+# Install protobuf
+
+sudo apt-get install g++
+sudo apt install curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+sudo apt update && sudo apt install bazel
+
+git clone https://github.com/protocolbuffers/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+bazel build :protoc :protobuf
+
+cd ..
+
 
 # Clone OpenTelemetry-cpp
 
-git clone -b v1.6.1 --recursive https://github.com/open-telemetry/opentelemetry-cpp
+git clone -b v1.9.0 --recurse-submodules https://github.com/open-telemetry/opentelemetry-cpp
 
 # Build/Install OpenTelemetry-cpp
 
@@ -37,7 +54,7 @@ cd opentelemetry-cpp
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_JAEGER=ON
+cmake ..  -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_OTLP_HTTP=ON
 cmake --build . --target all
 sudo cmake --install . --config RelWithDebInfo
 cd ../..
